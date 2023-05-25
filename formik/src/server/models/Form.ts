23ee36ef4d14749cd27a5fb3b18d9ed615eddc9f
Andrei Dobre <andrei.dobr3@gmail.com>
2023-05-26 00:48:05 +0300

    FOR-25 Created Form Schema

---
+++ formik/src/server/models/Form.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose"
+
+const FormSchema = new mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: mongoose.Types.ObjectId
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    fields: [{
+        type: {
+            type: String,
+            required: true
+        },
+        question: {
+            type: String,
+            required: true
+        },
+        options: [{
+            answer: {
+                type: String,
+            }
+        }]
+    }],
+    isDraft: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    creator: {
+        type: String,
+    }
+})
+
+export default mongoose.model('Form', FormSchema)
