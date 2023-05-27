10051644c69945d28e7454bce9bf9115123ccf74
Andrei Dobre <andrei.dobr3@gmail.com>
2023-05-27 13:57:09 +0300

    FOR-40 Create Answer Controller

--- formik/src/server/routers/root.ts
+++ formik/src/server/routers/root.ts
@@ -1,11 +1,13 @@
 import { publicProcedure, router } from '../trpc';
 import { forms } from './forms';
+import { answers } from './answers';
 
 export const appRouter = router({
   sayHi: publicProcedure.query(() => {
     return 'Hi from the server!'
   }),
-  forms
+  forms,
+  answers
 });
 
 export type AppRouter = typeof appRouter;
