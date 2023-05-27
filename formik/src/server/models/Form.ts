0db9a2485b6c962122d709cb7ceb6d3f402a42f5
Serban681 <serbands20@gmail.com>
2023-05-27 19:28:49 +0300

    FOR-53 Added delete button

--- formik/src/server/models/Form.ts
+++ formik/src/server/models/Form.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const FormSchema = new mongoose.Schema({
-    id: {
+    _id: {
         type: mongoose.Schema.Types.ObjectId,
         default: mongoose.Types.ObjectId
     },
