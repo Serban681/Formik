b4707f1bd0fe131cc6457acc40f84fb2b576309a
Serban681 <serbands20@gmail.com>
2023-06-01 00:03:57 +0300

    s

--- formik/src/utils/api.ts
+++ formik/src/utils/api.ts
@@ -3,6 +3,8 @@ import { createTRPCNext } from '@trpc/next';
 import type { AppRouter } from '@/server/routers/root';
 
 function getBaseUrl() {
+  if (typeof window !== "undefined") return "";
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
